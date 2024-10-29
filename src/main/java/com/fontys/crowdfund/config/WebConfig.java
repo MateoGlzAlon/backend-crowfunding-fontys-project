c6221368728ba934c66f003e12fb534ac85bf782
MateoGlzAlon <mateo.gonzalez.alonso@gmail.com>
2024-10-29 16:18:52 +0100

    feat: added prod link

--- src/main/java/com/fontys/crowdfund/config/WebConfig.java
+++ src/main/java/com/fontys/crowdfund/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // Allow all paths
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:3000", "https://raisehub.vercel.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
